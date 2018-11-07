517f45ce69b2f064236ea5934512a3aa9368fb16
gabrielcamr9 <is697747@iteso.mx>
2018-11-06 18:43:35 -0600

    Added Create Account Listener

--- app/src/main/java/com/example/gabriel/myapplication/LogInActivity.kt
+++ app/src/main/java/com/example/gabriel/myapplication/LogInActivity.kt
@@ -11,8 +11,14 @@ class LogInActivity  : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
         guestButton.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
             startActivity(intent)
         }
     }
